2407c7bdf48a6b7dd1413f0165950184bba948dd
vardanowna <anastaszakaryan@mail.ru>
2026-01-02 17:09:47 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
-const { YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer } = ymaps3;
+ymaps3.ready.then(() => {
+  const {
+    YMap,
+    YMapDefaultSchemeLayer,
+    YMapDefaultFeaturesLayer
+  } = ymaps3;
 
-const map = new YMap(
-  document.getElementById('map'),
-  {
-    location: {
-      center: [30.3158, 59.9391],
-      zoom: 9
+  const map = new YMap(
+    document.getElementById('map'),
+    {
+      location: {
+        center: [30.3158, 59.9391],
+        zoom: 9
+      }
     }
-  }
-);
+  );
 
-map.addChild(new YMapDefaultSchemeLayer({
-  customization: './customization.json'
-}));
+  map.addChild(
+    new YMapDefaultSchemeLayer({
+      customization: './customization.json'
+    })
+  );
 
-map.addChild(new YMapDefaultFeaturesLayer());
+  map.addChild(new YMapDefaultFeaturesLayer());
+});
