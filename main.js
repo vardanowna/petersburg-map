90d3e32205b51bc1c98e094b145a18836d7cbcd4
vardanowna <anastaszakaryan@mail.ru>
2026-01-02 13:00:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 const map = L.map('map', {
   zoomControl: false,
-  minZoom: 9,
-  maxZoom: 13
+  minZoom: 10,
+  maxZoom: 14
 }).setView([59.9386, 30.3141], 11);
 
 L.tileLayer(
-  'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-  { attribution: '' }
+  'https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=Vk2WuUzsjOcHeENR50mb',
+  {
+    tileSize: 512,
+    zoomOffset: -1
+  }
 ).addTo(map);
 
-const petersburgBounds = [
+const bounds = [
   [59.70, 29.30],
   [60.20, 30.80]
 ];
 
-map.setMaxBounds(petersburgBounds);
+map.setMaxBounds(bounds);
 map.on('drag', () => {
-  map.panInsideBounds(petersburgBounds, { animate: false });
+  map.panInsideBounds(bounds, { animate: false });
 });
