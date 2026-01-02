07a44cf904edabfb797c5c2ab79ad05745ffe59f
vardanowna <anastaszakaryan@mail.ru>
2026-01-02 13:27:51 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,56 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-  <meta charset="utf-8" />
-  <title>Карта Петербурга</title>
-  <style>
-    html, body, #map {
-      width: 100%;
-      height: 100%;
-      margin: 0;
+ymaps.ready(init);
+
+function init() {
+  const map = new ymaps.Map('map', {
+    center: [59.9386, 30.3141],
+    zoom: 11,
+    controls: []
+  });
+
+  map.setType('yandex#map');
+
+  map.options.set('styles', [
+    {
+      featureType: 'all',
+      elementType: 'geometry',
+      stylers: {
+        color: '#f5f7f9'
+      }
+    },
+    {
+      featureType: 'water',
+      elementType: 'geometry',
+      stylers: {
+        color: '#cfe3f1'
+      }
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry',
+      stylers: {
+        color: '#c2c7cc'
+      }
+    },
+    {
+      featureType: 'road',
+      elementType: 'labels',
+      stylers: {
+        color: '#7a7f85'
+      }
+    },
+    {
+      featureType: 'poi',
+      elementType: 'all',
+      stylers: {
+        visibility: 'off'
+      }
+    },
+    {
+      featureType: 'landscape.natural',
+      elementType: 'geometry',
+      stylers: {
+        color: '#eceff2'
+      }
     }
-  </style>
-  <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU"></script>
-</head>
-<body>
-  <div id="map"></div>
-  <script src="main.js"></script>
-</body>
-</html>
+  ]);
+}
