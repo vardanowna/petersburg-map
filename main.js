2ade8c674c19a4a21995290b5c6c34b6c43158dd
vardanowna <anastaszakaryan@mail.ru>
2026-01-02 12:24:11 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const map = L.map('map', {
-  zoomControl: false
+  zoomControl: false,
+  minZoom: 9,
+  maxZoom: 13
 }).setView([59.9386, 30.3141], 11);
 
 L.tileLayer(
-  'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+  'https://{s}.tile.openstreetmap.ru/{z}/{x}/{y}.png',
   { attribution: '' }
 ).addTo(map);
