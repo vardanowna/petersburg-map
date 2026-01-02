a9ccfa75d77a98048af4d463814703503a44e031
vardanowna <anastaszakaryan@mail.ru>
2026-01-02 12:25:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,13 @@ L.tileLayer(
   'https://{s}.tile.openstreetmap.ru/{z}/{x}/{y}.png',
   { attribution: '' }
 ).addTo(map);
+
+const petersburgBounds = [
+  [59.70, 29.30],
+  [60.20, 30.80]
+];
+
+map.setMaxBounds(petersburgBounds);
+map.on('drag', () => {
+  map.panInsideBounds(petersburgBounds, { animate: false });
+});
