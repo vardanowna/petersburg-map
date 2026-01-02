f2609def516cdf22d1ef4d93a86f93d8cff889b8
vardanowna <anastaszakaryan@mail.ru>
2026-01-02 17:21:28 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
-import customization from './customization.json' assert { type: 'json' };
+const init = async () => {
+  const response = await fetch('./customization.json');
+  const customization = await response.json();
 
-const { YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer } = ymaps3;
+  const { YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer } = ymaps3;
 
-const map = new YMap(
-  document.getElementById('map'),
-  {
-    location: {
-      center: [30.31413, 59.93863],
-      zoom: 9
+  const map = new YMap(
+    document.getElementById('map'),
+    {
+      location: {
+        center: [30.31413, 59.93863],
+        zoom: 9
+      }
     }
-  }
-);
+  );
 
-map.addChild(
-  new YMapDefaultSchemeLayer({
-    customization
-  })
-);
+  map.addChild(
+    new YMapDefaultSchemeLayer({
+      customization
+    })
+  );
 
-map.addChild(new YMapDefaultFeaturesLayer());
+  map.addChild(new YMapDefaultFeaturesLayer());
+};
+
+ymaps3.ready.then(init);
